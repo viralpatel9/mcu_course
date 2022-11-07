a3e36202c497d2e479c3b98330d4a6b3d5b12639
Viral Patel <108283708+viralpatel9@users.noreply.github.com>
2022-11-07 08:11:18 +0000

    datasheet added

--- stm32f4xx_drives/Debug/Src/main.d
+++ stm32f4xx_drives/Debug/Src/main.d
@@ -1,3 +1,3 @@
 Src/main.o: ../Src/main.c \
- C:/Mcu_course\ github/mcu_course/sttm32f4xx_drives/drivers/inc/stm32f407xx.h
-C:/Mcu_course\ github/mcu_course/sttm32f4xx_drives/drivers/inc/stm32f407xx.h:
+ C:/Mcu_course\ github/mcu_course/stm32f4xx_drives/drivers/inc/stm32f407xx.h
+C:/Mcu_course\ github/mcu_course/stm32f4xx_drives/drivers/inc/stm32f407xx.h:
--- stm32f4xx_drives/Debug/drivers/src/stm32f407xx_gpio_driver.d
+++ stm32f4xx_drives/Debug/drivers/src/stm32f407xx_gpio_driver.d
@@ -1,6 +1,6 @@
 drivers/src/stm32f407xx_gpio_driver.o: \
  ../drivers/src/stm32f407xx_gpio_driver.c \
- C:/Mcu_course\ github/mcu_course/sttm32f4xx_drives/drivers/inc/stm32f407xx_gpio_driver.h \
- C:/Mcu_course\ github/mcu_course/sttm32f4xx_drives/drivers/inc/stm32f407xx.h
-C:/Mcu_course\ github/mcu_course/sttm32f4xx_drives/drivers/inc/stm32f407xx_gpio_driver.h:
-C:/Mcu_course\ github/mcu_course/sttm32f4xx_drives/drivers/inc/stm32f407xx.h:
+ C:/Mcu_course\ github/mcu_course/stm32f4xx_drives/drivers/inc/stm32f407xx_gpio_driver.h \
+ C:/Mcu_course\ github/mcu_course/stm32f4xx_drives/drivers/inc/stm32f407xx.h
+C:/Mcu_course\ github/mcu_course/stm32f4xx_drives/drivers/inc/stm32f407xx_gpio_driver.h:
+C:/Mcu_course\ github/mcu_course/stm32f4xx_drives/drivers/inc/stm32f407xx.h:
