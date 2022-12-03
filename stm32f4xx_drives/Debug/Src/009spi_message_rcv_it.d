1e4b4d56423aa398fff9f4fd2fefc79eee9c32e3
Viral Patel <108283708+viralpatel9@users.noreply.github.com>
2022-12-03 22:06:26 +0000

    spi send message to slave

--- stm32f4xx_drives/Debug/Src/009spi_message_rcv_it.d
+++ stm32f4xx_drives/Debug/Src/009spi_message_rcv_it.d
@@ -1,4 +1,4 @@
-Src/008spi_cmd_handling.o: ../Src/008spi_cmd_handling.c \
+Src/009spi_message_rcv_it.o: ../Src/009spi_message_rcv_it.c \
  C:/Mcu_course\ github/mcu_course/stm32f4xx_drives/drivers/inc/stm32f407xx.h \
  C:/Mcu_course\ github/mcu_course/stm32f4xx_drives/drivers/inc/stm32f407xx_gpio_driver.h \
  C:/Mcu_course\ github/mcu_course/stm32f4xx_drives/drivers/inc/stm32f407xx.h \
--- stm32f4xx_drives/Debug/Src/subdir.mk
+++ stm32f4xx_drives/Debug/Src/subdir.mk
@@ -5,13 +5,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/008spi_cmd_handling.c 
+../Src/009spi_message_rcv_it.c 
 
 OBJS += \
-./Src/008spi_cmd_handling.o 
+./Src/009spi_message_rcv_it.o 
 
 C_DEPS += \
-./Src/008spi_cmd_handling.d 
+./Src/009spi_message_rcv_it.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -21,7 +21,7 @@ Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/008spi_cmd_handling.d ./Src/008spi_cmd_handling.o ./Src/008spi_cmd_handling.su
+	-$(RM) ./Src/009spi_message_rcv_it.d ./Src/009spi_message_rcv_it.o ./Src/009spi_message_rcv_it.su
 
 .PHONY: clean-Src
 
